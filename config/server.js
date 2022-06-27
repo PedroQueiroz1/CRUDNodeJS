825aaa71816da04f24395811ea2af065e7a5b62b
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-06-27 00:09:18 -0300

    Modifica server.js

--- config/server.js
+++ config/server.js
@@ -3,5 +3,10 @@ module.exports = function(){
     var express = require('express');
     
     var app = express();
+    app.set('view engine', 'ejs');
+    app.set('views','./app/views');
 
+    app.listen(8000,function(){
+        console.log("localhost:8000");
+    });
 }
